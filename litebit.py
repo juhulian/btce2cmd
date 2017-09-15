f7e89980d8476fd66fba916aa8c09db99b37cb12
Julian Rilli <julian@rilli.eu>
2017-09-16 01:04:35 +0200

    fix buy/sell mix-up :-)

--- litebit.py
+++ litebit.py
@@ -9,6 +9,6 @@ try:
     d = json.loads(urllib2.urlopen("https://api.litebit.eu/markets").read())["result"]
     print("EUR buy     sell   {}".format(strftime("%Y-%m-%d %H:%M:%S")))
     for c in ["btc", "bcc", "eth", "etc", "ltc", "xem"]:
-        print("{} {:07.2f} {:07.2f}".format(c, d[c]["sell"], d[c]["buy"]))
+        print("{} {:07.2f} {:07.2f}".format(c, d[c]["buy"], d[c]["sell"]))
 except Exception as err:
     print(err)
