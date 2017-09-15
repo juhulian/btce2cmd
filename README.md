b55106b5c7c0ed426aa9e4d4f571c32499482a27
Julian Rilli <julian@rilli.eu>
2017-09-16 01:00:58 +0200

    add litebit.eu script

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ btc: 2727.999 USD  2401.865 EUR
 ltc:   43.712 USD    38.523 EUR     0.016 BTC
 eth:  346.169 USD   306.472 EUR     0.127 BTC
 ```
+
+litebit api: buy and sell in EUR
+```bash
+$ python2 litebit.py
+EUR buy     sell   2017-09-16 00:57:05
+btc 3095.28 3259.27
+bcc 0340.08 0381.50
+eth 0209.87 0224.03
+etc 0009.29 0010.28
+ltc 0042.26 0046.88
+xem 0000.17 0000.19
+```
---
+++ litebit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import urllib2, json
+from time import strftime
+
+
+try:
+    d = json.loads(urllib2.urlopen("https://api.litebit.eu/markets").read())["result"]
+    print("EUR buy     sell   {}".format(strftime("%Y-%m-%d %H:%M:%S")))
+    for c in ["btc", "bcc", "eth", "etc", "ltc", "xem"]:
+        print("{} {:07.2f} {:07.2f}".format(c, d[c]["sell"], d[c]["buy"]))
+except Exception as err:
+    print(err)
