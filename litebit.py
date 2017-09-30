d9e8fe8a2fe12b6acf9b1e9dacfea38fe250ac35
Julian Rilli <julian@rilli.eu>
2017-09-30 16:31:05 +0200

    api changed, without a user-agent you get a HTTP Error 403: Forbidden

--- litebit.py
+++ litebit.py
@@ -4,11 +4,21 @@
 import urllib2, json
 from time import strftime
 
+url = "https://api.litebit.eu/markets"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+       #'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       #'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       #'Accept-Encoding': 'none',
+       #'Accept-Language': 'en-US,en;q=0.8',
+       #'Connection': 'keep-alive'}
+
+req = urllib2.Request(url, headers=hdr)
 
 try:
-    d = json.loads(urllib2.urlopen("https://api.litebit.eu/markets").read())["result"]
+    res = json.loads(urllib2.urlopen(req).read())["result"]
+except urllib2.HTTPError, e:
+    print e.fp.read()
+else:
     print("EUR buy     sell   {}".format(strftime("%Y-%m-%d %H:%M:%S")))
     for c in ["btc", "bcc", "eth", "etc", "ltc", "xem"]:
-        print("{} {:07.2f} {:07.2f}".format(c, d[c]["buy"], d[c]["sell"]))
-except Exception as err:
-    print(err)
+        print("{} {:07.2f} {:07.2f}".format(c, res[c]["buy"], res[c]["sell"]))
